7df0400f71ec6559aee9717d4c97dc9fc4e1f918
dericwong <dericwong@cyber-village.net>
2026-04-10 18:09:41 +0800

    Remove gFont to avoid third party call from the library

--- printing/lib/printing.dart
+++ printing/lib/printing.dart
@@ -16,14 +16,14 @@
 
 import 'package:pdf/widgets.dart';
 
-import 'src/fonts/gfonts.dart';
+// import 'src/fonts/gfonts.dart';
 
 export 'package:pdf_widget_wrapper/pdf_widget_wrapper.dart';
 
 export 'src/asset_utils.dart';
 export 'src/cache.dart';
 export 'src/callback.dart';
-export 'src/fonts/gfonts.dart';
+// export 'src/fonts/gfonts.dart';
 export 'src/output_type.dart';
 export 'src/preview/action_bar_theme.dart';
 export 'src/preview/actions.dart';
@@ -33,26 +33,26 @@ export 'src/printing.dart';
 export 'src/printing_info.dart';
 export 'src/raster.dart';
 
-Future<void> pdfDefaultTheme() async {
-  if (ThemeData.buildThemeData != null) {
-    return;
-  }
+// Future<void> pdfDefaultTheme() async {
+//   if (ThemeData.buildThemeData != null) {
+//     return;
+//   }
 
-  final base = await PdfGoogleFonts.openSansRegular();
-  final bold = await PdfGoogleFonts.openSansBold();
-  final italic = await PdfGoogleFonts.openSansItalic();
-  final boldItalic = await PdfGoogleFonts.openSansBoldItalic();
-  final emoji = await PdfGoogleFonts.notoColorEmoji();
-  final icons = await PdfGoogleFonts.materialIcons();
+//   final base = await PdfGoogleFonts.openSansRegular();
+//   final bold = await PdfGoogleFonts.openSansBold();
+//   final italic = await PdfGoogleFonts.openSansItalic();
+//   final boldItalic = await PdfGoogleFonts.openSansBoldItalic();
+//   final emoji = await PdfGoogleFonts.notoColorEmoji();
+//   final icons = await PdfGoogleFonts.materialIcons();
 
-  ThemeData.buildThemeData = () {
-    return ThemeData.withFont(
-      base: base,
-      bold: bold,
-      italic: italic,
-      boldItalic: boldItalic,
-      icons: icons,
-      fontFallback: [emoji, base],
-    );
-  };
-}
+//   ThemeData.buildThemeData = () {
+//     return ThemeData.withFont(
+//       base: base,
+//       bold: bold,
+//       italic: italic,
+//       boldItalic: boldItalic,
+//       icons: icons,
+//       fontFallback: [emoji, base],
+//     );
+//   };
+// }
